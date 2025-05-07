6ecdafa2aeb24598bfb929f96b35c4adc830629d
angel antonio lopez <angelantoniolopezpere864@gmail.com>
2025-05-07 14:21:33 -0600

    creacion del proyecto de fichas de internet usando vue y spring

---
+++ src/main/java/com/angel/fichas/de/internet/FichasDeInternetApplication.java
@@ -0,0 +1,13 @@
+package com.angel.fichas.de.internet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FichasDeInternetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FichasDeInternetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/angel/fichas/de/internet/FichasDeInternetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angel.fichas.de.internet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FichasDeInternetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
