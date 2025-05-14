4d264ec3bca79831006539d1b7be9ee28171bf85
angel antonio lopez <angelantoniolopezpere864@gmail.com>
2025-05-14 17:21:29 -0600

    Act12-Creando Metodo Get para traer la informacion de las fichas

--- fichas-de-internet/src/main/java/com/angel/fichas/de/internet/controllers/fichasController.java
+++ fichas-de-internet/src/main/java/com/angel/fichas/de/internet/controllers/fichasController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import com.angel.fichas.de.internet.repository.FichaRepository;
 
 @RestController
 @RequestMapping("/fichas")
+@CrossOrigin(origins = "*")
 public class fichasController {
 @Autowired
     private FichaRepository fichaRepository;
--- ftront_fichas/src/App.vue
+++ ftront_fichas/src/App.vue
@@ -1,5 +1,14 @@
 <script setup>
+import {ref, onMounted} from 'vue';
+import axios from 'axios';
 
+const fichas = ref([]);
+const cargarFichas = async () =>{
+const responde = await axios.get('http://localhost:8080/fichas/TraerFichas')
+fichas.value = responde.data;
+console.log(fichas.value);
+}
+onMounted(cargarFichas);
 </script>
 
 <template>
